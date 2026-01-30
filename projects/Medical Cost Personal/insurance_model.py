0fe1359bcfb1f9b5fc528cd4719170166f0e1981
Chyrus Miguel D. Macalla <macalla.chyrusmiguel@gmail.com>
2026-01-30 11:46:03 +0800

    Refactor insurance model to include smoker feature and improve prediction accuracy

--- projects/Medical Cost Personal/insurance_model.py
+++ projects/Medical Cost Personal/insurance_model.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 
 # Predicting the medical cost personal using Decision Tree Regressor
 
@@ -11,12 +13,13 @@ medical_cost_data = pd.read_csv(data_file_path)
 # === Data Cleaning ===
 
 medical_cost_data.dropna(axis=1) # Remove incomplete columns (Just in case)
+medical_cost_data["smoker"] = medical_cost_data["smoker"].map({"yes": 1, "no": 0})
 
 # === Getting the X and y values ===
 
 # Getting features for model to use in prediction (datas to feed)
 
-features = ['age','bmi', 'children']
+features = ['age','bmi', 'children', 'smoker']
 
 X = medical_cost_data[features]
 
@@ -25,19 +28,27 @@ y = medical_cost_data.charges
 
 # === Choosing a model ===
 
-insurance_model = DecisionTreeRegressor()
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1) # Splits data into training (study) and evaluation (examination); avoids model memorizing the answer key (overfitting)
+
+insurance_model = DecisionTreeRegressor(random_state=1)
 
 # Training our model
-insurance_model.fit(X,y)
+insurance_model.fit(train_X,train_y)
 
 # === Making Predictions === 
 
-predictions = insurance_model.predict(X)
+predictions = insurance_model.predict(val_X)
 
 # === Comparing the prediction === 
 
-print(predictions)
-print("===============")
-print(medical_cost_data.charges)
+print("Model's prediction:")
+print(predictions[:5])
+print()
+print("Target values:")
+print(y.head(5).to_list())
 
-# I guess it has some good accuracy? Some are correct and others don't?
+# === Calculating Mean Absolute Error === 
+
+MAE = mean_absolute_error(val_y, predictions)
+
+print(f"\nMean Absolute Error: {MAE}") # My guy just memorized the answer key lol
